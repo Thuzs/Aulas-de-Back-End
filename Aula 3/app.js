2438777c5b2d199f084655378fafc7767770e010
ARTHUR LIMA TRAUTWEIN <25203614@CFP127ALUNO.LOCAL>
2026-02-11 08:04:23 -0300

    Criação da entrada de dados

---
+++ Aula 3/app.js
@@ -0,0 +1,40 @@
+/**********************************************************
+ * Objetivo: Criar um sistema que permite calculo de juros
+ * utilizando boas práticas com funções
+ * Data: 11/02/2026
+ * Autor: Brayan
+ * Versão: 1.0 
+ **********************************************************/
+//Import da biblioteca do readline
+const readline = require('readline')
+
+//Criar o objeto de entrada de dados
+const entradaDeDados = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+//Entrada do nome do cliente
+entradaDeDados.question('Digite o nome do cliente: ', function(nome){
+    let nomeCliente = nome
+
+    //Entrada do nome do produto
+    entradaDeDados.question('Digite o nome do Produto: ', function(produto){
+        let nomeProduto = produto
+
+        //Entrada do valor da compra
+        entradaDeDados.question('Digite o valor da Compra: ', function(capital){
+            let capitalProduto = capital
+
+            //Entrada da taxa de juros
+            entradaDeDados.question('Digite a taxa de juros a ser aplicada na Compra: ', function(taxa){
+                let taxaCompra = taxa
+
+                //Entrada do tempo de pagamento
+                entradaDeDados.question('Digite o tempo para realizar o pagamento: ', function(tempo){
+                    let tempoPagamento = tempo
+                })
+            })
+        })
+    })
+})
