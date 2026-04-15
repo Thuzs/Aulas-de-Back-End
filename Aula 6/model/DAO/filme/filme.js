d6571ad98ec700f9433014d9af740e0c78eebe8f
ARTHUR LIMA TRAUTWEIN <25203614@CFP127ALUNO.LOCAL>
2026-04-15 11:30:09 -0300

    Primeira aula finalizada

---
+++ Aula 6/model/DAO/filme/filme.js
@@ -0,0 +1,70 @@
+/***************************************************************************
+ * Objetivo: Arquivo responsavel pelo CRUD no Banco de dados MySQL na tabela 
+ *           filme
+ * Data: 15/04/2026
+ * Autor: Arthur
+ * Versão: 1.0
+ ***************************************************************************/
+
+//Import da biblioteca para generenciar o anco de dados MySQL no node.js
+const knex = require('knex')
+
+//Import do arquivo de configuração para conexão com o BD MySQL
+const knexConfig = require('../../database_config_knex/knexFile.js')
+
+//Criar a conexão do banco de dados MySQL
+const knexConex = knex(knexConfig.development)
+
+//Função para inserir dados na tabela de filme
+const insertFilme = async function(filme){
+    let sql = `insert into tbl_filme (
+                nome, 
+                data_lancamento, 
+                duracao, 
+                sinopse, 
+                avaliacao, 
+                valor, 
+                capa
+                )
+        values (
+                '${filme.nome}',
+                '${filme.data_lancamento}',
+                '${filme.duracao}',
+                '${filme.sinopse}',
+                '${filme.avaliacao}',
+                '${filme.valor}',
+                '${filme.capa}'
+                );`
+
+    //Executar o scriptSQL no banco de dados
+    let result = await knexConex.raw(sql)
+
+    if(result)
+        return true
+    else
+        return false
+}
+
+//Função para atualizar um filme existente na tabela
+const updateFilme = async function(filme){
+}
+
+//Função para retornar todos os dados da tabela de filme
+const selectAllFilme = async function(){
+}
+
+//Função para retornar os dados do filme, filtrando pelo id
+const selectByIdFilme = async function(id){
+}
+
+//Função para excluir um filme pelo id
+const deleteFiltro = async function(id){
+}
+
+module.exports = {
+    insertFilme,
+    updateFilme,
+    selectAllFilme,
+    selectByIdFilme,
+    deleteFiltro
+}
